70d63b9f931e063b7f91d74080e34c5996f059d9
Ddragonever <ddragonever@gmail.com>
2017-02-24 23:17:34 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#IFE2017学习代码仓库
+
+####1、文件目录介绍
+* static/ 存放图片CSS等静态资源
+* templates/ 存放html文件
+
+####2、任务演示demo网址
+* [ife.jxnugo.com
+](ife.jxnugo.com)
+####3、任务更新情况
+* 小薇学院任务1: templates/task1.html
---
+++ run.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('profile.html')
+
+
+@app.route('/vivi-1')
+def task_1():
+    return render_template('task1.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
